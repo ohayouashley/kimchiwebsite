19e08d4c16d64b7a2bfab131beed37efb999d7a3
Ashley Chase <ashleychase@Ashleys-MacBook-Pro.local>
2021-05-09 14:28:38 -0500

    git commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// function myFunction(string1, string2)  {
+//     return string1 + string2
+// }
+
+// let resultOfFunction = myFunction("string1", "string2") 
+// console.log(resultOfFunction)
+
+function button(){
+    console.log("button")
+  } 
+
+
+
+
